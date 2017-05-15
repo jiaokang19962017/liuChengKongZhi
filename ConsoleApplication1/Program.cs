fafe12088b91984a454a60991c96e5f0e663ab81
jiaokang <Administrator@HP201-1>
2017-05-15 10:28:06 +0800

    if-else嵌套

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,23 +10,41 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //int i = 0,j=0;
-            //i = ++j ;
-            //Console.WriteLine(i);
-            //Console.WriteLine(j);
-            int score = Convert.ToInt32(Console.ReadLine());
-            if (score>=85) {
-                Console.WriteLine("优秀");
-            }else if(score>=75)
+            /*  
+              int i = 0,j=0;
+              i = ++j ;
+              Console.WriteLine(i);
+              Console.WriteLine(j);
+              */
+            /* int score = Convert.ToInt32(Console.ReadLine());
+             if (score>=85) {
+                 Console.WriteLine("优秀");
+             }else if(score>=75)
+             {
+                 Console.WriteLine("良好");
+
+             }else if (score>=60 ) {
+                 Console.WriteLine("及格");
+             }
+             else
+             {
+                 Console.WriteLine("不及格");
+             }*/
+            int score_a = 90;
+            int score_b = 90;
+            if (score_a >= score_b)
             {
-                Console.WriteLine("良好");
-               
-            }else if (score>=60 ) {
-                Console.WriteLine("及格");
+                if (score_a == score_b)
+                {
+                    Console.WriteLine("成绩相同{0}", score_a);
+                }
+                else
+                {
+                    Console.WriteLine("较高的成绩{0}", score_b);
+                }
             }
-            else
-            {
-                Console.WriteLine("不及格");
+            else {
+                Console.WriteLine("较高的成绩为{0}",score_b);
             }
         }
     }
